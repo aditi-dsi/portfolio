285eed1099109467a409b725ac2531ed44c55499
Aditi Bindal <123075271+aditi-dsi@users.noreply.github.com>
2023-10-26 18:24:10 +0530

    Update Portfolio.jsx

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -31,28 +31,22 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "iVolve-Realtime Chat Application",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "Real time Messaging Application with Authentication and Authorization using React, Node.js, Express and Socket.io",
+    url: "https://www.github.com/aditi-dsi/iVolve",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Data Analytics Portfolio",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "Full Report of all the projects performed during my internship at Trainity, their findings and insights.",
+    url: "https://drive.google.com/file/d/1ODHcwCe8jUhhSzMe3pPQI4QLyAT0a6_-/view?usp=sharing",
   },
   {
-    title: "My Resume Site",
+    title: "Download Resume",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
-  },
-  {
-    title: "GitHub Codespaces and github.dev",
-    description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "Here is my resume giving more comprehensive details about my skills and experience.",
+    url: "https://drive.google.com/file/d/1DrGXdGM7pjhrymyNKI_GpXVKXCYsLuhU/view?usp=sharing",
   },
 ];
 
