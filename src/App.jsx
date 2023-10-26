0e1191bae6645a82cebcd762b8cb27dc957de297
Aditi Bindal <123075271+aditi-dsi@users.noreply.github.com>
2023-10-26 16:26:18 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,18 +24,18 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Aditi Bindal",
+  title: "Full Stack Developer & AI/ML Practitioner",
+  email: "aditii.b6@gmail.com",
+  gitHub: "aditi-dsi",
+  instagram: "",
+  linkedIn: "aditi-bindal",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
-const primaryColor = "#4E567E";
+const primaryColor = "#94cab8";
 const secondaryColor = "#D2F1E4";
 
 const App = () => {
